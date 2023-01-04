7d1ef6f9d0d427078d0926b760e86f7c4578a2bf
murat.dogan2 <murat.dogan2@ERKHBN0096>
2023-01-04 16:23:09 +0300

    Add project files.

---
+++ 170822_DialogPencereleri/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace _170822_DialogPencereleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDenemeTahtasi = new System.Windows.Forms.Label();
+            this.btnFontDialog = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnOpenFileDialog = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSaveFileDialog = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnFolderBrowser = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColorDialog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDenemeTahtasi
+            // 
+            this.lblDenemeTahtasi.AutoSize = true;
+            this.lblDenemeTahtasi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDenemeTahtasi.Location = new System.Drawing.Point(316, 224);
+            this.lblDenemeTahtasi.Name = "lblDenemeTahtasi";
+            this.lblDenemeTahtasi.Size = new System.Drawing.Size(165, 25);
+            this.lblDenemeTahtasi.TabIndex = 0;
+            this.lblDenemeTahtasi.Text = "Merhaba Dünya";
+            // 
+            // btnFontDialog
+            // 
+            this.btnFontDialog.Location = new System.Drawing.Point(26, 30);
+            this.btnFontDialog.Name = "btnFontDialog";
+            this.btnFontDialog.Size = new System.Drawing.Size(170, 33);
+            this.btnFontDialog.TabIndex = 1;
+            this.btnFontDialog.Text = "Font Dıalog";
+            this.btnFontDialog.UseVisualStyleBackColor = true;
+            this.btnFontDialog.Click += new System.EventHandler(this.btnFontDialog_Click);
+            // 
+            // btnOpenFileDialog
+            // 
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(223, 30);
+            this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(147, 35);
+            this.btnOpenFileDialog.TabIndex = 2;
+            this.btnOpenFileDialog.Text = "Open File Dialog";
+            this.btnOpenFileDialog.UseVisualStyleBackColor = true;
+            this.btnOpenFileDialog.Click += new System.EventHandler(this.btnOpenFileDialog_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "C:\\Users\\murat.dogan2\\Desktop";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(316, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Merhaba Dünya";
+            // 
+            // btnSaveFileDialog
+            // 
+            this.btnSaveFileDialog.Location = new System.Drawing.Point(389, 30);
+            this.btnSaveFileDialog.Name = "btnSaveFileDialog";
+            this.btnSaveFileDialog.Size = new System.Drawing.Size(147, 35);
+            this.btnSaveFileDialog.TabIndex = 3;
+            this.btnSaveFileDialog.Text = "Save File Dialog";
+            this.btnSaveFileDialog.UseVisualStyleBackColor = true;
+            this.btnSaveFileDialog.Click += new System.EventHandler(this.btnSaveFileDialog_Click);
+            // 
+            // btnFolderBrowser
+            // 
+            this.btnFolderBrowser.Location = new System.Drawing.Point(557, 30);
+            this.btnFolderBrowser.Name = "btnFolderBrowser";
+            this.btnFolderBrowser.Size = new System.Drawing.Size(208, 36);
+            this.btnFolderBrowser.TabIndex = 4;
+            this.btnFolderBrowser.Text = "Folder Browser Dialog";
+            this.btnFolderBrowser.UseVisualStyleBackColor = true;
+            this.btnFolderBrowser.Click += new System.EventHandler(this.btnFolderBrowser_Click);
+            // 
+            // btnColorDialog
+            // 
+            this.btnColorDialog.Location = new System.Drawing.Point(263, 113);
+            this.btnColorDialog.Name = "btnColorDialog";
+            this.btnColorDialog.Size = new System.Drawing.Size(272, 37);
+            this.btnColorDialog.TabIndex = 5;
+            this.btnColorDialog.Text = "Color Dialog";
+            this.btnColorDialog.UseVisualStyleBackColor = true;
+            this.btnColorDialog.Click += new System.EventHandler(this.btnColorDialog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnColorDialog);
+            this.Controls.Add(this.btnFolderBrowser);
+            this.Controls.Add(this.btnSaveFileDialog);
+            this.Controls.Add(this.btnOpenFileDialog);
+            this.Controls.Add(this.btnFontDialog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDenemeTahtasi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDenemeTahtasi;
+        private System.Windows.Forms.Button btnFontDialog;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnOpenFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSaveFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnFolderBrowser;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnColorDialog;
+    }
+}
+
---
+++ 170822_DialogPencereleri/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _170822_DialogPencereleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFontDialog_Click(object sender, EventArgs e)
+        {
+            //Bütün dialog pencerelerini Show veya showdialog isminde metotları bulunur.
+            //Bu metotlar dialog pencerelerini açmayı sağlar.
+           DialogResult sonuc = fontDialog1.ShowDialog();
+            if (sonuc== DialogResult.OK)
+            {
+                lblDenemeTahtasi.Font = fontDialog1.Font;
+            }
+        }
+
+        private void btnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\Users\murat.dogan2\Desktop";
+            openFileDialog1.Filter = "Jpeg Dosyası|*.jpeg|Jpg Dosyası|*.jpg|Png Dosyası|*.png";  
+           DialogResult sonuc= openFileDialog1.ShowDialog();
+            if (sonuc==DialogResult.OK)
+            {
+                lblDenemeTahtasi.Text = openFileDialog1.FileName;//dosya ismi ve dizin yolunu verir
+
+                label1.Text = openFileDialog1.SafeFileName;//Sadece dosya yolunu verir
+            }
+        }
+
+        private void btnSaveFileDialog_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Kaydedilecek Dosyayı Seçiniz";
+            DialogResult sonuc= saveFileDialog1.ShowDialog();
+            if (sonuc==DialogResult.OK)
+            {
+                lblDenemeTahtasi.Text = saveFileDialog1.FileName;
+            }
+            
+        }
+
+        private void btnFolderBrowser_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc=folderBrowserDialog1.ShowDialog();//klasör seçtirir
+            if (sonuc==DialogResult.OK)
+            {
+                lblDenemeTahtasi.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnColorDialog_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc = colorDialog1.ShowDialog();
+            if (sonuc==DialogResult.OK)
+            {
+                lblDenemeTahtasi.ForeColor = colorDialog1.Color;
+                label1.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
